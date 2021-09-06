24b00c90857f6484c02877919a4f2d9a5bc025fb
Abishovyusif <89875188+Abishovyusif@users.noreply.github.com>
2021-09-06 14:10:41 +0400

    play.py

--- modules/play.py
+++ modules/play.py
@@ -96,7 +96,7 @@ async def play(client, message, current_client):
                             song_info['title'], song_info['thumbnails'][-1], cover_file_name)
 
                 # download and process the song
-                sent_msg = await sent_msg.edit(f"**__ 🙃 Yüklənir.Biraz gözlə qadası [Təxmini : 10 saniyə]__**")
+                sent_msg = await sent_msg.edit(f"**__ {req_by} istədiyin `{(song_info['title'].strip())[:20]} mahnısını yükləyirəm🎶 __**")
                 filename = await DownloaderService.download_and_transcode_song(f"{song_info['link']}")
                 if filename is None:
                     m = await sent_msg.edit(f"**__✖️ Mahnı adını düzgüb yazdığına əmin ol! __**")
