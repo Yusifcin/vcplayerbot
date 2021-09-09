7deab841a13b077accd455ab077ed55d3f129f45
Abishovyusif <89875188+Abishovyusif@users.noreply.github.com>
2021-09-09 21:19:12 +0400

    GenerateCover.py

--- helpers/GenerateCover.py
+++ helpers/GenerateCover.py
@@ -51,12 +51,12 @@ async def generate_cover(title, thumbnail, result_file_name):
         img = Image.open(f"images/{temp_file}.png")
         draw = ImageDraw.Draw(img)
 
-        draw.text((10, 580), f"@VoiceMahni_bot", fill="white", font=ITC_REG)
+        draw.text((10, 580), f"Dark Music", fill="white", font=ITC_REG)
 
         draw.text((10, 640), f"{title}", fill="white", font=KRONA_52)
 
-        draw.text((985, 20), f"YushkaMusic", fill="white", font=KRONA_SMALL)
-        draw.text((1100, 50), f"012", fill="white", font=KRONA_SMALL)
+        draw.text((985, 20), f"Xoş", fill="white", font=KRONA_SMALL)
+        draw.text((1100, 50), f"Dinləmələr", fill="white", font=KRONA_SMALL)
 
         img.save(result_file_name, optimize=True, quality=20)
         final_img = result_file_name
